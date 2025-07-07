c7a622f1a9927df5e31032f64beba8c0c7381284
André <andre.anberg@gmail.com>
2025-07-07 21:02:39 +0200

    skapat sätt att duktigt förlänga bilden

--- main.py
+++ main.py
@@ -6,14 +6,7 @@ image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 height, width = np.shape(image)
 
-vertical_slices = 4
-horizontal_slices = 3
-
-chunk_height = height / vertical_slices
-chunk_width = width / horizontal_slices
-
-def extend(image, x=0, y=0):
-    height, width = np.shape(image)
+def _extend(image, x=0, y=0):
     for x in range(x):
         i = 0 if x % 2 == 0 else -1
         image = np.insert(image,i,image[:,i],axis=1)
@@ -22,20 +15,23 @@ def extend(image, x=0, y=0):
         image = np.insert(image,i,image[i,:],axis=0)
     return image
 
-#print(arr)
-print(extend(arr,2))
-image = extend(image, 10,10)
+def fit(image, vertical_slices=1, horizontal_slices=1):
+    assert(vertical_slices>0)
+    assert(horizontal_slices>0)
+    
+    height, width = np.shape(image)
+    chunk_height = np.ceil(height / vertical_slices)
+    chunk_width = np.ceil(width / horizontal_slices)
+    v_diff = int(chunk_height * vertical_slices - height)
+    h_diff = int(chunk_width * horizontal_slices - width)
+    return _extend(image, v_diff, h_diff)
+
+image = fit(image, 3, 1)
+print(np.shape(image))
 
 #image[1:100, 1:100] = np.clip(image[1:100, 1:100] * 0, 0, 255)
 
 
-
-"""
-def cut(image, x_slices = 1, y_slices = 1):
-    height, width = np.shape(image)
-    if height % 2 != 0:
-        image[:,:]
-"""
 
 
 
