b9c6f53e504fe86ef0cd8dd00abb9534159bab9f
André <andre.anberg@gmail.com>
2025-07-07 20:52:46 +0200

    lyckades med att förlänga bilden åt alla håll

--- main.py
+++ main.py
@@ -1,14 +1,46 @@
-# import opencv
+import numpy as np
 import cv2
- 
-# Read the image
+
 image = cv2.imread('image.jpg')
-# grayscale the image
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+height, width = np.shape(image)
+
+vertical_slices = 4
+horizontal_slices = 3
+
+chunk_height = height / vertical_slices
+chunk_width = width / horizontal_slices
+
+def extend(image, x=0, y=0):
+    height, width = np.shape(image)
+    for x in range(x):
+        i = 0 if x % 2 == 0 else -1
+        image = np.insert(image,i,image[:,i],axis=1)
+    for y in range(y):
+        i = 0 if y % 2 == 0 else -1
+        image = np.insert(image,i,image[i,:],axis=0)
+    return image
+
+#print(arr)
+print(extend(arr,2))
+image = extend(image, 10,10)
+
+#image[1:100, 1:100] = np.clip(image[1:100, 1:100] * 0, 0, 255)
+
+
+
+"""
+def cut(image, x_slices = 1, y_slices = 1):
+    height, width = np.shape(image)
+    if height % 2 != 0:
+        image[:,:]
+"""
+
+
+
+
  
-cv2.imshow('Original Image', image)
- 
-cv2.imshow('Grayscale Image', gray_image)
+cv2.imshow('', image)
 cv2.waitKey(0)
- 
 cv2.destroyAllWindows()
