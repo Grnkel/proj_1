887096f70a7195ae6d7d6506831243ec774362ee
aanberg <andre.anberg@volvocars.com>
2025-07-09 16:59:29 +0200

    bra rounding och tips på bättre

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ image = cv2.imread('image.jpg')
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 height, width = np.shape(image)
 
-vertical_slices = 10
-horizontal_slices = 10
+vertical_slices = 50
+horizontal_slices = 50
+
+print("the shape of the image:", np.shape(image))
 
 def extend(im, x=0, y=0):
+    assert(x>0)
+    assert(y>0)
+
     for x in range(x):
         i = 0 if x % 2 == 0 else -1
         im = np.insert(im,i,im[:,i],axis=1)
@@ -22,17 +27,24 @@ def chunk_shape(im, v_slices=1, h_slices=1):
     assert(h_slices>0)
 
     height, width = np.shape(im)
-    ch_height = np.ceil(height / v_slices)
-    ch_width = np.ceil(width / h_slices)
-    return int(ch_height), int(ch_width)
+    ch_height = int(np.ceil(height / v_slices))
+    ch_width = int(np.ceil(width / h_slices))
+    return ch_height, ch_width
 
 def fit(im, v_slices=1, h_slices=1):
     assert(v_slices>0)
     assert(h_slices>0)
 
     ch_height, ch_width = chunk_shape(im, v_slices, h_slices)
-    v_diff = int(ch_height * v_slices - np.shape(im)[0])
-    h_diff = int(ch_width * h_slices - np.shape(im)[1])
+    v_diff = ch_height * v_slices - np.shape(im)[0]
+    h_diff = ch_width * h_slices - np.shape(im)[1]
+
+    height, width = np.shape(im)
+    r_v_slice = int(np.ceil(height / np.ceil(height / v_slices)))
+    r_h_slice = int(np.ceil(width / np.ceil(width / h_slices)))
+
+    print("recommended slices:", "v:", r_v_slice, "h:", r_h_slice)
+    print("diffs:", v_diff, h_diff)
     return extend(image, v_diff, h_diff)
 
 image = fit(image, vertical_slices, horizontal_slices)
@@ -41,11 +53,10 @@ for row in range(vertical_slices):
     for col in range(horizontal_slices): 
         ROW = slice(row * ch_h, (row + 1) * ch_h)
         COL = slice(col * ch_w, (col + 1) * ch_w)
-        image[ROW, COL] = image[ROW, COL] * 0
+        image[ROW, COL] = np.sum(image[ROW, COL]) / (ch_h*ch_w)
 
-        # show changes gradually
-        cv2.imshow('', image)
-        cv2.waitKey(1)#np.max(1, int(1000 / (vertical_slices * horizontal_slices))))
+        #cv2.imshow('', image)
+        #cv2.waitKey(1)
 
 # TODO skapa klass för skiten
 # TODO behöver kind of motsatsen, börja med en form och få antal slices + fit
