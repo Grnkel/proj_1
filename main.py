9eda3938aaad728a442c12719eea703fa6d29f1a
Andre Anberg <andre.anberg@gmail.com>
2025-07-09 16:22:17 +0200

    gjorde den typ av iterering jag var ute efter

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ image = cv2.imread('image.jpg')
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 height, width = np.shape(image)
 
-vertical_slices = 1
-horizontal_slices = 1
+vertical_slices = 10
+horizontal_slices = 10
 
 def extend(im, x=0, y=0):
     for x in range(x):
@@ -24,7 +24,7 @@ def chunk_shape(im, v_slices=1, h_slices=1):
     height, width = np.shape(im)
     ch_height = np.ceil(height / v_slices)
     ch_width = np.ceil(width / h_slices)
-    return ch_height, ch_width
+    return int(ch_height), int(ch_width)
 
 def fit(im, v_slices=1, h_slices=1):
     assert(v_slices>0)
@@ -35,13 +35,20 @@ def fit(im, v_slices=1, h_slices=1):
     h_diff = int(ch_width * h_slices - np.shape(im)[1])
     return extend(image, v_diff, h_diff)
 
-#def select(im, v_slice, h_slice): TODO skapa klass för skiten
+image = fit(image, vertical_slices, horizontal_slices)
+ch_h, ch_w = chunk_shape(image, vertical_slices, horizontal_slices)
+for row in range(vertical_slices):
+    for col in range(horizontal_slices): 
+        ROW = slice(row * ch_h, (row + 1) * ch_h)
+        COL = slice(col * ch_w, (col + 1) * ch_w)
+        image[ROW, COL] = image[ROW, COL] * 0
+
+        # show changes gradually
+        cv2.imshow('', image)
+        cv2.waitKey(1)#np.max(1, int(1000 / (vertical_slices * horizontal_slices))))
+
+# TODO skapa klass för skiten
 # TODO behöver kind of motsatsen, börja med en form och få antal slices + fit
-
-image= extend(image, 200, 200)
-#image = fit(image, vertical_slices, horizontal_slices)
-
-print(np.shape(image))
  
 cv2.imshow('', image)
 cv2.waitKey(0)
