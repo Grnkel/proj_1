e321b65d3f388f0e60f2e0a969488618827518bc
Andre Anberg <andre.anberg@gmail.com>
2025-07-11 16:24:26 +0200

    några kommentarer för optimeringen

--- discrete.py
+++ discrete.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 # TODO matroid theory används kanske eller annan diskret optimering för att hitta
+# TODO integer programming visade sig vara bättre, men om jag vill tillåta 
+# vissa chunks att vara större än andra så kan jag använda dynamic programming
 
 def fit_chunk(im_height, im_width, width, height):
 
