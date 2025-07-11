fb4fe98a9f5ebcf91f1d82d1846a0e75482f6e62
aanberg <andre.anberg@volvocars.com>
2025-07-11 16:04:57 +0200

    skapat idéer och fick in en matris

---
+++ ascii.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2
+
+ascii_matrix = [
+    [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/'],
+    ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?'],
+    ['@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O'],
+    ['P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_'],
+    ['`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o'],
+    ['p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~', '^']
+]
+
+ascii_dict = {
+    char: (y, x)
+    for y, row in enumerate(ascii_matrix)
+    for x, char in enumerate(row)
+}
+
+
+for row in ascii_matrix:
+    print(row)
