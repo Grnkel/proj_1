c5519b2ea5d4affe5e0c0a0d0c1f88d0d27181ad
André <andre.anberg@gmail.com>
2025-07-22 21:31:54 +0200

    snygg offset lösning skapad och man kan faktiskt få skiten ner i terminalen, dock lite hårdkodad

--- terminal.py
+++ terminal.py
@@ -19,32 +19,34 @@ print(f"Width: {width}, Height: {height}")
 class TerminalHandler(ImageHandler):
     def __init__(self, path):
         super().__init__(path)
-        # TODO fixa skiten så den är automatisk!
+        self.grayscale()
+
         self.ascii = Ascii("chars/font12x16.png")
         self.ascii.generate_list()
         
-        super().__init__(path)
         height, width = self.ascii.chunk_dims
-        self.fit_chunk(height, width)
+        self.fit_chunk(height, width+2) # sliding window effect
         print(self.chunks)
         print(self.slices)
             
         v_slices, h_slices = self.slices
         self.matrix = np.full((v_slices, h_slices) , '', dtype='<U1') 
 
+        
+    
+    def to_terminal(self):
         for i,j,chunk in super().__iter__():
             index = np.sum(chunk) / self.chunk_pixels / 256 / 3
             char = self.ascii.sorted[int(index*len(self.ascii.sorted))]
             self.matrix[i][j] = char
-    
-    def to_terminal(self):
+
         os.system('clear')
         for row in self.matrix:
             print(' '.join(row))
 
 def main():
-    terminal = TerminalHandler('images/image1.jpg')
-    terminal.apply(func=partial(terminal.contrast,1,0.5))
+    terminal = TerminalHandler('images/image4.jpg')
+    terminal.apply(func=partial(terminal.contrast,15,0.8))
     terminal.show()
     terminal.to_terminal()
 
